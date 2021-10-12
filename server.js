1ef338f29d5fc9f349363766dc642bf7ee5b49ba
Walid <walid@reveout.com>
2021-10-12 13:59:29 +0100

    main update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ async function main() {
   const favoriteFood = process.env.FAVORITE_FOOD;
   console.log(`My favorite food is ${favoriteFood}`);
   while(true) {
-    console.log("Microservices rock!");
+    console.log("Microservices rock!!!");
     await sleep(5000);
   }
 }
